f2bd6a27ec6d527c94e4df3fbe8688323893c36c
Emily Ye <emilyye@google.com>
2019-08-12 20:17:14 -0700

    test

--- .ci/magic-modules/create-pr.sh
+++ .ci/magic-modules/create-pr.sh
@@ -46,8 +46,17 @@ LABELS=""
 
 # Check the files between this commit and HEAD
 # If they're only contained in third_party, add the third_party label.
-if [ ! git diff --name-only HEAD^1 | grep -v "third_party" | grep -v ".gitmodules" | grep -r "build/" ]; then
+if [ -z "$(git diff --name-only HEAD^1 | grep -v "third_party" | grep -v ".gitmodules" | grep -r "build/")" ]; then
   LABELS="${LABELS}only_third_party,"
+fi
+
+echo "$(git diff --name-only HEAD^1)"
+VALIDATOR_WARN_FILES="$(git diff --name-only HEAD^1 | grep -Ff '.ci/magic-modules/vars_validator_handwritten_files.txt' | sed 's/^/* /')"
+echo "${VALIDATOR_WARN_FILES}"
+
+if [ -n "${VALIDATOR_WARN_FILES}" ]; then
+  MESSAGE="${MESSAGE}${NEWLINE}**WARNING**: The following files may need corresponding changes in third_party/validator:"
+  MESSAGE="${MESSAGE}${NEWLINE}${VALIDATOR_WARN_FILES}${NEWLINE}"
 fi
 
 # Terraform
